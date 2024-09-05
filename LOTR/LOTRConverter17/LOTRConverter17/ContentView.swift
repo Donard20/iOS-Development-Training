1b77783a9d4cb0d8223d79a8769c22cdb2b824ad
Donard Azura <90747937+Donard20@users.noreply.github.com>
2024-09-05 22:50:11 +0800

    Lesson 1
    Create new Xcode Project
    Add app icons and images

---
+++ LOTR/LOTRConverter17/LOTRConverter17/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  LOTRConverter17
+//
+//  Created by DONARD on 9/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Button("Button") {
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTR/LOTRConverter17/LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by DONARD on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
